37c0c5f4cc1bcf32f7f6127098724eca7b6677ae
Sergei Sorokin <serebanyshka7@gmail.com>
2018-08-05 23:42:16 +0300

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
-
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -13,8 +12,16 @@ const io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  
   console.log('New user connected');
+  
+  socket.on('createMessage', (message) => {
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    }); //event for everybody
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
